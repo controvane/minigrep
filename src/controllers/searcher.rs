0c691f0d7eaea8f288dd67670ffb2fcbd9f9082c
Carlos Javier Murguia Ayo <cmurguia@exalogicsti.com>
2026-08-18 15:53:07 -0400

    Modify so terms are only computed once

--- src/controllers/searcher.rs
+++ src/controllers/searcher.rs
@@ -7,63 +7,33 @@ use std::process;
 
 fn apply_or_filters(
     lines: impl Iterator<Item = (String, String)>,
-    terms: Vec<String>,
+    terms: &[String],
 ) -> impl Iterator<Item = (String, String)> {
     return lines.filter(move |(_orig, lower)| terms.iter().any(|p| lower.contains(p)));
 }
 
 fn apply_and_filters(
     lines: impl Iterator<Item = (String, String)>,
-    terms: Vec<String>,
+    terms: &[String],
 ) -> impl Iterator<Item = (String, String)> {
     return lines.filter(move |(_orig, lower)| terms.iter().all(|p| lower.contains(p)));
 }
 
 fn apply_exclusions(
     lines: impl Iterator<Item = (String, String)>,
-    terms: Vec<String>,
+    terms: &[String],
 ) -> impl Iterator<Item = (String, String)> {
     return lines.filter(move |(_orig, lower)| !terms.iter().any(|p| lower.contains(p)));
 }
 
 pub fn search(arguments: Arguments) {
-    match arguments.get_path() {
-        InputSource::Normal(path) => {
-            if path.is_file() {
-                let reader = match File::open(path) {
-                    Ok(file) => Box::new(BufReader::new(file)),
-                    Err(_) => {
-                        println!("File was either not found or inexistent.");
-                        process::exit(1);
-                    }
-                };
-                search_on_buffer(&arguments, reader);
-                return;
-            }
-            for file_path in walk(path) {
-                let reader = match File::open(file_path) {
-                    Ok(file) => Box::new(BufReader::new(file)),
-                    Err(_) => {
-                        println!("File was either not found or inexistent.");
-                        continue;
-                    }
-                };
-                search_on_buffer(&arguments, reader);
-            }
-        }
-        InputSource::Stdin => {
-            let reader: Box<dyn BufRead> = Box::new(BufReader::new(io::stdin()));
-            search_on_buffer(&arguments, reader);
-        }
-    };
-}
+    let case_insensitive = arguments.get_case_insensitive();
 
-fn search_on_buffer(arguments: &Arguments, reader: Box<dyn BufRead>) {
     let or_terms: Vec<String> = arguments
         .get_search_terms_or()
         .iter()
         .map(|x| {
-            if arguments.get_case_insensitive() {
+            if case_insensitive {
                 return x.to_lowercase();
             } else {
                 return x.clone();
@@ -75,7 +45,7 @@ fn search_on_buffer(arguments: &Arguments, reader: Box<dyn BufRead>) {
         .get_search_terms_and()
         .iter()
         .map(|x| {
-            if arguments.get_case_insensitive() {
+            if case_insensitive {
                 return x.to_lowercase();
             } else {
                 return x.clone();
@@ -87,7 +57,7 @@ fn search_on_buffer(arguments: &Arguments, reader: Box<dyn BufRead>) {
         .get_search_terms_ex()
         .iter()
         .map(|x| {
-            if arguments.get_case_insensitive() {
+            if case_insensitive {
                 return x.to_lowercase();
             } else {
                 return x.clone();
@@ -95,9 +65,46 @@ fn search_on_buffer(arguments: &Arguments, reader: Box<dyn BufRead>) {
         })
         .collect();
 
+    match arguments.get_path() {
+        InputSource::Normal(path) => {
+            if path.is_file() {
+                let reader = match File::open(path) {
+                    Ok(file) => Box::new(BufReader::new(file)),
+                    Err(_) => {
+                        println!("File was either not found or inexistent.");
+                        process::exit(1);
+                    }
+                };
+                search_on_buffer(case_insensitive, reader, &or_terms, &and_terms, &ex_terms);
+                return;
+            }
+            for file_path in walk(path) {
+                let reader = match File::open(file_path) {
+                    Ok(file) => Box::new(BufReader::new(file)),
+                    Err(_) => {
+                        println!("File was either not found or inexistent.");
+                        continue;
+                    }
+                };
+                search_on_buffer(case_insensitive, reader, &or_terms, &and_terms, &ex_terms);
+            }
+        }
+        InputSource::Stdin => {
+            let reader: Box<dyn BufRead> = Box::new(BufReader::new(io::stdin()));
+            search_on_buffer(case_insensitive, reader, &or_terms, &and_terms, &ex_terms);
+        }
+    };
+}
+
+fn search_on_buffer<'a>(
+    case_insensitive: bool,
+    reader: Box<dyn BufRead>,
+    or_terms: &'a [String],
+    and_terms: &'a [String],
+    ex_terms: &'a [String],
+) {
     let base_lines = reader.lines().map_while(Result::ok);
-    let mut lines: Box<dyn Iterator<Item = (String, String)>> = if arguments.get_case_insensitive()
-    {
+    let mut lines: Box<dyn Iterator<Item = (String, String)> + 'a> = if case_insensitive {
         Box::new(base_lines.map(|line| {
             let lower = line.to_lowercase();
             return (line, lower);
@@ -109,15 +116,15 @@ fn search_on_buffer(arguments: &Arguments, reader: Box<dyn BufRead>) {
         }))
     };
 
-    if arguments.get_search_terms_or().len() > 0 {
+    if or_terms.len() > 0 {
         lines = Box::new(apply_or_filters(lines, or_terms));
     }
 
-    if arguments.get_search_terms_and().len() > 0 {
+    if and_terms.len() > 0 {
         lines = Box::new(apply_and_filters(lines, and_terms));
     }
 
-    if arguments.get_search_terms_ex().len() > 0 {
+    if ex_terms.len() > 0 {
         lines = Box::new(apply_exclusions(lines, ex_terms));
     }
 
